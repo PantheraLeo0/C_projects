e190e1979a99cff6e8ae3bdc6ed85428bbbc8b92
nishant.chaudhary <nishantchaudhary2078@gmail.com>
2023-03-26 11:13:27 +0545

    Length Converter

--- unit_converter.c
+++ unit_converter.c
@@ -2,9 +2,211 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include "unit_converter_functions.c"
 
 int main(){
-    printf("Choose the unit to unit : \n");
-    printf("1.")
+    int option;
+    home : 
+    printf("Choose the unit to unit option(1-7) : \n\n");
+    printf("1.Length\n");
+    printf("2.Temperature\n");
+    printf("3.Area\n");
+    printf("4.Volume\n");
+    printf("5.Weight\n");
+    printf("6.Time\n");
+    printf("7.Exit\n");
+    scanf("%d",&option);
+    system("cls");
+    if(option == 1){
+        printf("Choose the unit to ....(1-6) : \n\n");
+        printf("1.Centimeter to ...\n");
+        printf("2.Meter to ...\n");
+        printf("3.Kilometer to ...\n");
+        printf("4.Mile to ...\n");
+        printf("5.Yard to ...\n");
+        printf("6.Foot to ...\n");
+        scanf("%d",&option);
+        system("cls");
+        if(option == 1){
+            printf("1.Centimeter to Meter\n");
+            printf("2.Centimeter to Millimeter\n"); 
+            printf("3.Centimeter to Kilometer\n");  
+            printf("4.Centimeter to Mile\n");
+            printf("5.Centimeter to Yard\n");
+            printf("6.Centimeter to Foot\n"); 
+            scanf("%d",&option);
+            system("cls");
+            switch(option){
+                case 1: centi_to_meter();
+                goto home;
+                break;
+                case 2: centi_to_millimeter();
+                goto home;
+                break;
+                case 3: centi_to_kilometer();
+                goto home;
+                break;
+                case 4: centi_to_mile();
+                goto home;
+                break;
+                case 5: centi_to_yard();
+                goto home;
+                break;
+                case 6: centi_to_foot();
+                goto home;
+                break;
+            }
+        }
+        if(option == 2){
+            printf("1.Meter to Centimeter\n");
+            printf("2.Meter to Millimeter\n");
+            printf("3.Meter to Kilometer\n");
+            printf("4.Meter to Mile\n");
+            printf("5.Meter to Yard\n");
+            printf("6.Meter to Foot\n\n");
+            scanf("%d",&option);
+            system("cls");
+            switch(option){
+                case 1: meter_to_centimeter();
+                goto home;
+                break;
+                case 2: meter_to_millimeter();
+                goto home;
+                break;
+                case 3: meter_to_kilometer();
+                goto home;
+                break;
+                case 4: meter_to_mile();
+                goto home;
+                break;
+                case 5: meter_to_yard();
+                goto home;
+                break;
+                case 6: meter_to_foot();
+                goto home;
+                break;
+            }
+        }
+        if(option == 3){
+            printf("1.Kilometer to Centimeter\n");
+            printf("2.Kilometer to Meter\n");
+            printf("3.Kilometer to Millimeter\n");
+            printf("4.Kilometer to Mile\n");
+            printf("5.Kilometer to Yard\n");
+            printf("6.Kilometer to Foot\n\n");
+            scanf("%d",&option);
+            system("cls");
+            switch(option){
+                case 1: kilometer_to_centimeter();
+                goto home;
+                break;
+                case 2: kilometer_to_millimeter();
+                goto home;
+                break;
+                case 3: kilometer_to_meter();
+                goto home;
+                break;
+                case 4: kilometer_to_mile();
+                goto home;
+                break;
+                case 5: kilometer_to_yard();
+                goto home;
+                break;
+                case 6: kilometer_to_foot();
+                goto home;
+                break;
+            }
+        }
+        if(option == 4){
+            printf("1.Mile to Centimeter\n");
+            printf("2.Mile to Meter\n");
+            printf("3.Mile to Kilometer\n");
+            printf("4.Mile to Yard\n");
+            printf("5.Mile to Foot\n");
+            printf("6.Mile to millimeter\n\n");
+            scanf("%d",&option);
+            system("cls");
+            switch(option){
+                case 1: mile_to_centimeter();
+                goto home;
+                break;
+                case 2: mile_to_millimeter();
+                goto home;
+                break;
+                case 3: mile_to_meter();
+                goto home;
+                break;
+                case 4: mile_to_kilometer();
+                goto home;
+                break;
+                case 5: mile_to_yard();
+                goto home;
+                break;
+                case 6: mile_to_foot();
+                goto home;
+                break;
+            }
+        }
+        if(option == 5){
+            printf("1.Yard to Centimeter\n");
+            printf("2.Yard to Meter\n");
+            printf("3.Yard to Kilometer\n");
+            printf("4.Yard to Millimeter\n");
+            printf("5.Yard to Mile\n");
+            printf("6.Yard to Foot\n\n");
+            scanf("%d",&option);
+            system("cls");
+            switch(option){
+                case 1: yard_to_centimeter();
+                goto home;
+                break;
+                case 2: yard_to_millimeter();
+                goto home;
+                break;
+                case 3: yard_to_meter();
+                goto home;
+                break;
+                case 4: yard_to_mile();
+                goto home;
+                break;
+                case 5: yard_to_kilometer();
+                goto home;
+                break;
+                case 6: yard_to_foot();
+                goto home;
+                break;
+            }
+        }
+        if(option == 6){
+            printf("1.Foot to Centimeter\n");
+            printf("2.Foot to Meter\n");
+            printf("3.Foot to Kilometer\n");
+            printf("4.Foot to Millimeter\n");
+            printf("5.Foot to Mile\n");
+            printf("6.Foot to Yard\n\n");
+            scanf("%d",&option);
+            system("cls");
+            switch(option){
+                case 1: Foot_to_centimeter();
+                goto home;
+                break;
+                case 2: Foot_to_millimeter();
+                goto home;
+                break;
+                case 3: Foot_to_meter();
+                goto home;
+                break;
+                case 4: Foot_to_mile();
+                goto home;
+                break;
+                case 5: Foot_to_yard();
+                goto home;
+                break;
+                case 6: Foot_to_kilometer();
+                goto home;
+                break;
+            }
+        }
+    }
     return 0;
 }
---
+++ unit_converter_functions.c
@@ -0,0 +1,130 @@
+// Functions to convert centimeter to ...
+void centi_to_meter(){
+    float centimeter_value;
+    printf("Centimeter : ");
+    scanf("%f",&centimeter_value);
+    printf("%.3f centimeter is %.3f meter\n\n",centimeter_value,0.01 * centimeter_value);
+}
+
+void centi_to_millimeter(){
+    float centimeter_value;
+    printf("Centimeter : ");
+    scanf("%f",&centimeter_value);
+    printf("%.3f centimeter is %.3f millimeter\n\n",centimeter_value,10 * centimeter_value);
+}
+
+void centi_to_kilometer(){
+    float centimeter_value;
+    printf("Centimeter : ");
+    scanf("%f",&centimeter_value);
+    printf("%.3f centimeter is %f kilometer\n\n",centimeter_value,0.00001 * centimeter_value);
+}
+
+void centi_to_mile(){
+    float centimeter_value;
+    printf("Centimeter : ");
+    scanf("%f",&centimeter_value);
+    printf("%.3f centimeter is %.10f mile\n\n",centimeter_value,0.0000062137 * centimeter_value);
+}
+
+void centi_to_yard(){
+    float centimeter_value;
+    printf("Centimeter : ");
+    scanf("%f",&centimeter_value);
+    printf("%.3f centimeter is %.10f yard\n\n",centimeter_value,0.010936133 * centimeter_value);
+}
+
+void centi_to_foot(){
+    float centimeter_value;
+    printf("Centimeter : ");
+    scanf("%f",&centimeter_value);
+    printf("%.3f centimeter is %.10f foot\n\n",centimeter_value,0.032808399 * centimeter_value);
+}
+
+
+// Functions to convert meter to ...
+void meter_to_centimeter(){
+    float meter_value;
+    printf("Meter : ");
+    scanf("%f",&meter_value);
+    printf("%.3f meter is %.3f centimeter\n\n",meter_value,100 * meter_value);
+}
+
+void meter_to_millimeter(){
+    float meter_value;
+    printf("Meter : ");
+    scanf("%f",&meter_value);
+    printf("%.3f Meter is %.3f millimeter\n\n",meter_value,1000 * meter_value);
+}
+
+void meter_to_kilometer(){
+    float meter_value;
+    printf("Meter : ");
+    scanf("%f",&meter_value);
+    printf("%.3f Meter is %.3f kilometer\n\n",meter_value,0.001 * meter_value);
+}
+
+void meter_to_mile(){
+    float meter_value;
+    printf("Meter : ");
+    scanf("%f",&meter_value);
+    printf("%.3f Meter is %.10f mile\n\n",meter_value,0.0006213689 * meter_value);
+}
+
+void meter_to_yard(){
+    float meter_value;
+    printf("Meter : ");
+    scanf("%f",&meter_value);
+    printf("%.3f Meter is %.10f yard\n\n",meter_value,1.0936132983 * meter_value);
+}
+
+void meter_to_foot(){
+    float meter_value;
+    printf("Meter : ");
+    scanf("%f",&meter_value);
+    printf("%.3f Meter is %.10f foot\n\n",meter_value,3.280839895 * meter_value);
+}
+
+
+//functions to convert kilometer to ...
+void kilometer_to_centimeter(){
+    float kilometer_value;
+    printf("Kilometer : ");
+    scanf("%f",&kilometer_value);
+    printf("%.3f kilometer is %.3f centimeter\n\n",kilometer_value,100000 * kilometer_value);
+}
+
+void kilometer_to_millimeter(){
+    float kilometer_value;
+    printf("kilometer : ");
+    scanf("%f",&kilometer_value);
+    printf("%.3f kilometer is %.3f millimeter\n\n",kilometer_value,1000000 * kilometer_value);
+}
+
+void kilometer_to_meter(){
+    float kilometer_value;
+    printf("kilometer : ");
+    scanf("%f",&kilometer_value);
+    printf("%.3f kilometer is %.3f meter\n\n",kilometer_value,1000 * kilometer_value);
+}
+
+void kilometer_to_mile(){
+    float kilometer_value;
+    printf("kilometer : ");
+    scanf("%f",&kilometer_value);
+    printf("%.3f kilometer is %.10f mile\n\n",kilometer_value,0.6213688756 * kilometer_value);
+}
+
+void kilometer_to_yard(){
+    float kilometer_value;
+    printf("kilometer : ");
+    scanf("%f",&kilometer_value);
+    printf("%.3f kilometer is %.10f yard\n\n",kilometer_value,1093.6132983 * kilometer_value);
+}
+
+void kilometer_to_foot(){
+    float kilometer_value;
+    printf("kilometer : ");
+    scanf("%f",&kilometer_value);
+    printf("%.3f kilometer is %.10f foot\n\n",kilometer_value,3280.839895 * kilometer_value);
+}
