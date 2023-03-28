a9bd9953c153843018ee786827935ccda9715c66
nishant.chaudhary <nishantchaudhary2078@gmail.com>
2023-03-29 02:57:38 +0545

    Students Management System

--- students_records.c
+++ students_records.c
@@ -5,6 +5,7 @@
 #include <windows.h>
 
 FILE *key_buffer;
+FILE *buffer_area;
 
 void password(void);
 void display(void);
@@ -22,13 +23,17 @@ int main(){
 }
 
 void check_key(){
-    char key[100], temp_key[100];
+    char key[] = "password";
+    char temp_key[100];
 
     printf("Password : ");
     scanf("%s",key);
 
-    key_buffer = fopen("current_key","r");
+    key_buffer = fopen("current.txt","w");
+    fprintf(buffer_area,"%s",key);
+    fclose(buffer_area);
 
+    key_buffer = fopen("current_key.txt","r");
     while(fscanf(key_buffer,"%s",temp_key) != EOF){
         if(strcmp(key,temp_key) == 0){
             printf("Access Granted");
@@ -84,8 +89,64 @@ void display(){
 }
 
 void addFun(){
-    
+    int s_class;
+    char s_name[100], s_address[100];
+    long int s_phone;
+    char decision;
+
+    do{
+        printf("Class : ");
+        scanf("%d",&s_class);
+        printf("Student Name : ");
+        scanf("%s",s_name);
+        printf("Student Address : ");
+        scanf("%s",s_address);
+        printf("Student Contact : ");
+        scanf("%ld",&s_phone);
+
+        buffer_area = fopen("records.txt","w");
+
+        fprintf(buffer_area,"%d %s %s %ld",s_class,s_name,s_address,s_phone);
+
+        printf("Data entered successfully\n\n");
+        printf("Do you want to add data(y/n) : ");
+        scanf("%c",&decision);
+        system("cls");
+    }while(decision == 'y');
+    fclose(buffer_area);
+    display();
 }
+
+void listFun(){
+    int s_class;
+    char s_name[100], s_address[100];
+    long int s_phone;
+    char temp;
+
+    buffer_area = fopen("records.txt","r");
+
+    printf("\tClass\tName\t\t\t\tAddress\t\t\t\tContact\n\n");
+    while(fscanf(buffer_area,"%d %s %s %ld",&s_class,s_name,s_address,s_phone) != EOF){
+        printf("\t%d\t%s\t\t\t%s\t\t\t%ld\n",s_class,s_name,s_address,s_phone);
+    }
+    fclose(buffer_area);
+    printf("Press 'D' for Menu : ");
+    scanf("%c",&temp);
+    if(temp == 'D'){
+        display();
+    }
+}
+
+void modifyFun(){
+    int s_class;
+    char s_name[100];
+
+    printf("Class : ");
+    scanf("%d",&s_class);
+    printf("Name : ");
+    scanf("%s",s_name);
+}
+
 void change_key(){
     char current_key[100], temp_current_key[100];
     char new_key[100];
